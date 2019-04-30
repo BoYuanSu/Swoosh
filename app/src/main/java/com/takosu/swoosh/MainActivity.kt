f51098a5bfa07943274838dd7d686108d7841366
su <mayday15221@gmail.com>
2019-04-30 15:35:48 +0800

    lesson-03

--- app/src/main/java/com/takosu/swoosh/MainActivity.kt
+++ app/src/main/java/com/takosu/swoosh/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.takosu.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGetStarted.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            this.startActivity(leagueIntent)
+        }
     }
 }
